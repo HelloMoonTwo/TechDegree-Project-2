681f717f4940aa35f7a06e4149c97c204dae2dee
HelloMoonTwo <fciscol360@gmail.com>
2019-01-07 21:11:11 -0800

    Adding base files for the app
    Adding the files needed to the repository.

---
+++ TechDegree-Project-2-QuizApp/QuizApp/QuizQuestions.swift
@@ -0,0 +1,7 @@
+//
+//  QuizQuestions.swift
+//  QuizApp
+//
+//  Created by Francisco Luna on 1/7/19.
+//  Copyright © 2019 Francisco Luna. All rights reserved.
+//
---
+++ TechDegree-Project-2-QuizApp/QuizApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizApp
+//
+//  Created by Francisco Luna on 1/7/19.
+//  Copyright © 2019 Francisco Luna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
